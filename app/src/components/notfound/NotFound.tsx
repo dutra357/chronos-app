e12e5db34cc16f36794947850f7d8fd80c37257f
David Alves Dutra <dadutra@hotmail.com>
2025-05-28 08:35:16 -0300

    ABOUT: Componente sobre/about para pomodoro.

--- app/src/components/notfound/NotFound.tsx
+++ app/src/components/notfound/NotFound.tsx
@@ -2,17 +2,18 @@ import style from './NotFound.css'
 
 import { Container } from '../../components/container/Container';
 import { MainTemplate } from '../../templates/MainTemplate/MainTemplate';
+import { GenericHtml } from '../generichtml/GenericHtml';
 
 export function NotFound() {
 
     return (
         <MainTemplate>
-
             <Container>
-                <h1>404 - Not Found</h1>
-                <p>ERRO ao carregar página!</p>
+                <GenericHtml>
+                    <h1>Erro: 404</h1>
+                    <h2>Page not found</h2>
+                </GenericHtml>
             </Container>
-
         </MainTemplate>
     )
 }

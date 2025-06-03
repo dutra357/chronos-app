c73c020b9baa121e7c3e21de92d8a862f9d48ae0
David Alves Dutra <dadutra@hotmail.com>
2025-06-03 19:00:43 -0300

    HISTORY: Estilização das th com css. Implementação da função de ordenação para as keys de TH.

--- app/src/components/pages/history/History.tsx
+++ app/src/components/pages/history/History.tsx
@@ -8,14 +8,37 @@ import styles from './History.module.css';
 import { useTaskContext } from '../../../contexts/TaskContext/UseTaskContext';
 import { formatDate } from '../../../utils/FormatDate';
 import { getTaskStatus } from '../../../utils/GetTaskStatus';
+import { useState } from 'react';
+import { sortTasks, type SortTasksOptions } from '../../../utils/SortTasks';
 
 
 export function History() {
 
     const { state } = useTaskContext();
 
-    const sortedTasks = [...state.tasks]
-        .sort((a, b) => b.startedAt - a.startedAt);
+    const [sortTaskOptions, setSortTaskOptions] = useState<SortTasksOptions>(() => {
+        return {
+            tasks: sortTasks({ tasks: state.tasks }),
+            field: 'startedAt',
+            direction: 'desc'
+        }
+    });
+
+    function handleSort({ field }: Pick<SortTasksOptions, 'field'>) {
+        const newDirection = sortTaskOptions.direction === 'asc' ? 'desc' : 'asc';
+
+        setSortTaskOptions({
+            tasks: sortTasks({
+                direction: newDirection,
+                tasks: sortTaskOptions.tasks,
+                field: field,
+            }),
+
+            field,
+            direction: newDirection
+        })
+    }
+
 
     return (
         <MainTemplate>
@@ -40,16 +63,23 @@ export function History() {
                     <table>
                         <thead>
                             <tr>
-                                <th>Tarefa</th>
-                                <th>Duração</th>
-                                <th>Data</th>
+                                <th onClick={() => handleSort({ field: 'name' })}
+                                    className={styles.thSort}>Tarefa ↕</th>
+
+                                <th onClick={() => handleSort({ field: 'duration' })}
+                                    className={styles.thSort}>Duração ↕</th>
+
+                                <th onClick={() => handleSort({ field: 'startedAt' })}
+                                    className={styles.thSort}>Data ↕</th>
+
                                 <th>Status</th>
+
                                 <th>Tipo</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                            {sortedTasks.map((task) => {
+                            {sortTaskOptions.tasks.map((task) => {
                                 const taskTypeMap = {
                                     workTime: 'Foco',
                                     shortBreakTime: 'Descanso curto',

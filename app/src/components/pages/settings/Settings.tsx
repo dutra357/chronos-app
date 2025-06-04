8eb2270be07bddb532594852055d5570c4792294
David Alves Dutra <dadutra@hotmail.com>
2025-06-04 10:06:46 -0300

    SETTINGS: Validação dos inputs não controlados. Adicionadas propriedades de controle, tipo e filtro para input html/css.

--- app/src/components/pages/settings/Settings.tsx
+++ app/src/components/pages/settings/Settings.tsx
@@ -6,10 +6,13 @@ import { Heading } from "../../heading/Heading";
 import { InputDefault } from "../../input/InputDefault";
 import { useRef } from "react";
 import { useTaskContext } from "../../../contexts/TaskContext/UseTaskContext";
+import { showMessage } from "../../../adapters/ToastfyAdapter";
 
 export function Settings() {
 
     const { state } = useTaskContext();
+
+    const errors: string[] = [];
 
     const workTimeInputRef = useRef<HTMLInputElement | null>(null);
     const shortBreakTimeInputRef = useRef<HTMLInputElement | null>(null);
@@ -17,12 +20,34 @@ export function Settings() {
 
     function handleSaveSettings(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        showMessage.dismiss();
 
-        const workTime = workTimeInputRef.current?.value;
-        const shortBreakTime = shortBreakTimeInputRef.current?.value;
-        const longBreakTime = longBreakTimeInputRef.current?.value;
+        const workTime = Number(workTimeInputRef.current?.value);
+        const shortBreakTime = Number(shortBreakTimeInputRef.current?.value);
+        const longBreakTime = Number(longBreakTimeInputRef.current?.value);
 
+        if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+            errors.push('Preencha os campos apenas com números.')
+        }
 
+        if (longBreakTime < 1 || longBreakTime > 30) {
+            errors.push('O tempo de descanso longo deve estar entre 1 e 30 minutos.')
+        }
+
+        if (shortBreakTime < 1 || shortBreakTime > 15) {
+            errors.push('O tempo de descanso curto deve estar entre 1 e 15 minutos.');
+        }
+
+        if (workTime < 1 || workTime > 20) {
+            errors.push('O tempo de foco deve estar entre 1 e 20 minutos.');
+        }
+
+        if (errors.length > 0) {
+            errors.forEach(error => {
+                showMessage.error(error);
+            });
+            return;
+        }
     }
 
     return (
@@ -38,14 +63,21 @@ export function Settings() {
             </Container>
 
             <Container>
-                <form action='' className='form' onSubmit={handleSaveSettings}>
+                <form action=''
+                    className='form'
+                    onSubmit={handleSaveSettings}>
 
                     <div className="formControl">
                         <InputDefault
                             id='workTime'
                             labelText='Tempo de foco'
                             ref={workTimeInputRef}
-                            defaultValue={state.config.workTime} />
+                            defaultValue={state.config.workTime}
+                            type='number' 
+                            min='1'
+                            max='20'
+                            step='1' 
+                            maxLength={2} />
                     </div>
 
                     <div className="formControl">
@@ -53,7 +85,12 @@ export function Settings() {
                             id='shortBreakTime'
                             labelText='Configurar descanso curto'
                             ref={shortBreakTimeInputRef}
-                            defaultValue={state.config.shortBreakTime} />
+                            defaultValue={state.config.shortBreakTime}
+                            type='number' 
+                            min='1'
+                            max='15'
+                            step='1' 
+                            maxLength={2} />
                     </div>
 
                     <div className="formControl">
@@ -61,7 +98,12 @@ export function Settings() {
                             id='longBreakTime'
                             labelText='Configurar descanso longo'
                             ref={longBreakTimeInputRef}
-                            defaultValue={state.config.longBreakTime} />
+                            defaultValue={state.config.longBreakTime}
+                            type='number' 
+                            min='1'
+                            max='30'
+                            step='1' 
+                            maxLength={2} />
                     </div>
 
                     <div className="formControl">
